57e583968dda1e91fca28cca13eaca5c7b7a217a
minvruslan <minvruslan@gmail.com>
2026-01-15 04:08:12 +0400

    Change basic template

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, watch } from 'vue';
+import MyComponent from './components/MyComponent.vue'
+
+const fullName = ref("")
+
+watch(
+  () => fullName.value,
+  (newVal: string) => {
+    console.log(newVal)
+  }
+)
+
+const onSave = (fullName: string) => {
+  console.log(fullName)
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <MyComponent v-model="fullName" @save="onSave" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const inputModel = defineModel<string>({ default: "" })
+
+const emit = defineEmits<{
+  (e: "save", fullName: string): void
+}>()
+
+const onSaveClick = () => {
+  emit("save", inputModel.value)
+}
+</script>
+
+<template>
+  <div class="my-component">
+    <span>Your full name: </span>
+    <input v-model="inputModel" type="text" class="my-component__input" />
+    <button class="my-component__button" @click="onSaveClick">Save</button>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.my-component {
+  padding: 20px;
+  width: 100%;
+  box-sizing: border-box;
+}
+</style>
